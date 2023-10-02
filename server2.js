91b80057dd8423219bf16133df89033217b2ddca
loganPasternak67854 <114943604+loganPasternak67854@users.noreply.github.com>
2023-10-02 12:19:54 -0400

    comments added to server2.js

--- server2.js
+++ server2.js
@@ -1,10 +1,23 @@
-const express = require('express');
+/*
+Express: provides broad features for building web and mobile applications. 
+It is used to build a single page, multipage, and hybrid web application. 
+It's a layer built on the top of the Node js that helps manage servers and routes.
+*/
+const express = require('express'); 
 const app = express();
+/*
+The Path module provides a way of working with directories and file paths.
+*/
 const path = require('path');
 
 const port = 8080; // Port number to listen on
 
 // Serve static files from the "public" directory
+/*
+A static file is any file that is stored in a server is the same every time
+it is delivered to users. HTML files,css files, and images are examples of 
+this kind of content.
+*/
 app.use(express.static(path.join(__dirname)));
 
 // Serve the "Testbed Website.html" file
